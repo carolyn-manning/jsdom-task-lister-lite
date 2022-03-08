06a89bf17a520e1fd1137de01950184b142eb27a
carolyn-manning <carolyn.manning3@gmail.com>
2022-03-08 13:23:42 -0500

    commit tree d973339bea0acb75f07753211ef037819ba53cbf

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const taskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  // const newTaskPriority = document.getElementById("new-task-priority");
+
+  const tasksList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const taskItem = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    taskItem.innerText = newTaskDescription.value;
+    deleteButton.innerText= "X";
+    deleteButton.dataset.action = "delete";
+    taskItem.appendChild(deleteButton)
+    tasksList.appendChild(taskItem);
+    taskForm.reset()
+  });
+
+  tasksList.addEventListener("click", function(e) {
+    if (e.target.dataset.action === "delete") {
+      e.target.parentElement.remove()
+    };
+
+  });
+
+  
 });
